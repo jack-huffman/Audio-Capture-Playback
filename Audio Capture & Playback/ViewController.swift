2fc9745aecfb0d07f08edd0bc03b9243fc07c4a0
Jack Huffman <jack.huffman17@gmail.com>
2019-04-09 18:13:03 -0500

    UI finished and ViewController started

--- Audio Capture & Playback/ViewController.swift
+++ Audio Capture & Playback/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
+    let audioSession = AVAudioSession.sharedInstance()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        requestRecordingPermission()
+        
     }
 
-
+    @IBAction func recordButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    func requestRecordingPermission() {
+        audioSession.requestRecordPermission() {
+            [unowned self] allowed in
+            if allowed {
+                // proceed to do whatever is needed to record
+            } else {
+                self.alertUser(title: "Recording Forbidden", message: "This app will not be allowed to record audio, and thus will not function properly.")
+            }
+        }
+    }
+    
+    func alertUser(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+        alert.present(self, animated: true, completion: nil)
+    }
+    
 }
 
